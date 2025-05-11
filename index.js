477b15e58c1ac0be3944ee6491e2d57d682d203c
Dhana Balan K <113673544+DhanaBalan2001@users.noreply.github.com>
2025-05-11 19:59:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,19 +29,12 @@ import feedbackRoutes from './routes/feedback.js';
 
 dotenv.config();
 const app = express();
-const server = http.createServer(app);
-const PORT = process.env.PORT || 5000;
 
 // Initialize Socket.io
 initSocket(server);
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
-  credentials: true
-}));
-app.use(express.json());
-app.use(httpLogger);
   origin: ['https://voluble-cendol-68fbbc.netlify.app'],
   credentials: true
 }));
@@ -91,6 +84,7 @@ mongoose.connect(process.env.MONGODB)
     console.error('MongoDB connection error:', err);
     process.exit(1);
   });
+
 mongoose.connection.on("disconnected", () => {
   console.log("MongoDB disconnected!");
 });
